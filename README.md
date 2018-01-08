d41240df0c4808c757d4dfa6adaf6cc1eb881f0b
Daniel Fredriksson <daniel.fredriksson@outlook.com>
2018-01-08 12:38:57 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tw_if_tcher
+An application to see if certain streamers are live streaming on Twitch.
+Using Twitch API to get data.
+Sorts out the relevant data and outputs it to HTML elements.
+
+Possible improvement: implement search function, to look for specific streamers.
--- Tw_if_tcher.js
+++ Tw_if_tcher.js
@@ -15,19 +15,23 @@ $(document).ready(function () {
     return streamerLink;
   }
   getStreamerLink();
-  
+
   //Function to add link to JSON command and get stream info from user.
-  function getStreamersInfo (streamerLinkIndex) {
+  function getStreamersInfo (streamerLinkIndex, displayBlockHTML, displayActivityHTML) {
     $.getJSON(streamerLinkIndex, function(data) {
       var streamTwitch = data.stream;
-      console.log(streamTwitch);
-
+      var streamTwitchChannel = streamTwitch.channel;
+      if (streamTwitch.stream_type == "live") {
+        $(displayBlockHTML).addClass("block--display");
+        $(displayActivityHTML).html("<img src=" + streamTwitchChannel.logo + " class='block--display-profilepicture'/>" + "<a href=" + streamTwitchChannel.url + " target='_blank' class='block--display-username'>" + streamTwitchChannel.display_name + " <span style='font-size: 10px;'>is playing: </span>" + streamTwitch.game + "</a>");
+      }
     });
   }
-  getStreamersInfo(streamerLink[0]);
+  getStreamersInfo(streamerLink[0], "#displayBlock1", "#displayActivity1");
+  getStreamersInfo(streamerLink[1], "#displayBlock2", "#displayActivity2");
+  getStreamersInfo(streamerLink[2], "#displayBlock3", "#displayActivity3");
+  getStreamersInfo(streamerLink[3], "#displayBlock4", "#displayActivity4");
+  getStreamersInfo(streamerLink[4], "#displayBlock5", "#displayActivity5");
+  getStreamersInfo(streamerLink[5], "#displayBlock6", "#displayActivity6");
+  getStreamersInfo(streamerLink[6], "#displayBlock7", "#displayActivity7");
 });
-  //Call function
-
-    //if (streamTwitch === "null") {
-      //("#Target").html("User is offline");
-    //}
